f34b180f65f106a1793826ab6bc7b38b8b657197
gintinks <luhutginting@gmail.com>
2023-03-07 13:13:03 +0800

    push

--- sort.js
+++ sort.js
@@ -1,55 +1,63 @@
 const array_mov = [
     {
         "movie_id": 1,
-        "picture" : "plane.jpg",
+        "picture": "plane.jpg",
         "title": "Plane",
         "rating": 76,
         "releasedate": "1971-08-01T00:00:00+00:00"
     },
     {
         "movie_id": 2,
-        "picture" : "theboys.jpg",
+        "picture": "theboys.jpg",
         "title": "The Boys",
         "rating": 62,
         "releasedate": "1971-08-01T00:00:00+00:00"
     },
     {
         "movie_id": 3,
-        "picture" : "tlor.jpg",
+        "picture": "tlor.jpg",
         "title": "The Lord of the Rings: The Return of the King",
         "rating": 89,
         "releasedate": "1971-08-01T00:00:00+00:00"
     },
     {
         "movie_id": 4,
-        "picture" : "godfather.jpg",
+        "picture": "godfather.jpg",
         "title": "The Godfather",
         "rating": 41,
         "releasedate": "1971-08-01T00:00:00+00:00"
     },
     {
         "movie_id": 5,
-        "picture" : "bandit.jpg",
+        "picture": "bandit.jpg",
         "title": "Bandit",
         "rating": 69,
         "releasedate": "1971-08-01T00:00:00+00:00"
     },
     {
         "movie_id": 6,
-        "picture" : "megan.jpg",
+        "picture": "megan.jpg",
         "title": "Megan",
         "rating": 50,
         "releasedate": "1971-08-01T00:00:00+00:00"
     },
 
 ];
+let data = [];
+
+const sortMovies = (movies, sortDirection) => {
+    const sortedMovies = movies.sort((a, b) => {
+        const compareValue = parseFloat(sortDirection === "asc" ? a.rating - b.rating : b.rating - a.rating)
+        return compareValue;
+    })
+
+    return sortedMovies;
+}
 
 const handleSortASC = () => {
     document.getElementById("accordion").innerHTML = "";
     const container = document.getElementById('accordion');
-    const data = array_mov.sort((a, b) => parseFloat(b.rating) - parseFloat(a.rating));
-    console.log(array_mov.sort((a, b) => parseFloat(a.rating) - parseFloat(b.rating)))
-
+    data = sortMovies(array_mov, 'asc');
     data.forEach((result) => {
         // Create card element
         const card = document.createElement('div');
@@ -83,9 +91,35 @@ const handleSortASC = () => {
     })
 }
 const handleSortDES = () => {
-    console.log("DES");
-    console.log(array_mov.sort((a, b) => parseFloat(b.rating) - parseFloat(a.rating)))
+    document.getElementById("accordion").innerHTML = "";
+    const container = document.getElementById('accordion');
+    data = sortMovies(array_mov, 'des');
+    data.forEach((result) => {
+        // Create card element
+        const card = document.createElement('div');
+        card.classList = 'card-body';
+
+
+        const content = `
+          <div class="card">
+            <div class="card-header" id="heading-${result.movie_id}">
+                ....
+            </div>
+        
+            <div id="collapse-${result.movie_id}" class="collapse show" aria-labelledby="heading-${result.movie_id}" data-parent="#accordion">
+                <div class="card-body">
+                <img style="width: 150px;" src=${result.picture} alt="" srcset="">
+                <h5>${result.title}</h5>
+                <p>${result.releasedate}</p>
+                <p>${result.rating}</p>
+                </div>
+            </div>
+          </div>
+        `;
+
+        // Append newyly created card element to the container
+        container.innerHTML += content;
+    })
 }
 
 handleSortASC()
-handleSortDES()
